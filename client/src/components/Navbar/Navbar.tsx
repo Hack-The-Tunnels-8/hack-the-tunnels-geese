775890cf90500dacf8583e9d39a6a337ea8825d7
DarkSabe <jaden.chang29@gmail.com>
2023-09-30 20:17:21 +0000

    updated navbar component

--- client/src/components/Navbar/Navbar.tsx
+++ client/src/components/Navbar/Navbar.tsx
@@ -1,4 +1,4 @@
-import { Link } from "react-router-dom";
+//import { Link } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
 import { useAccountContext } from "../../context";
 import "./Navbar.style.scss";
